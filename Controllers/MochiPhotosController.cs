74e12bb3bdce9e0b8e989728d05552bd01cf8fe1
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-25 16:01:59 -0700

    fix: fix edit post request

--- Controllers/MochiPhotosController.cs
+++ Controllers/MochiPhotosController.cs
@@ -93,35 +93,33 @@ namespace Mochilog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,PicTakenDate,UploadDate,ImageFileName")] MochiPhoto mochiPhoto)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,PicTakenDate,UploadDate")] MochiPhoto mochiPhoto, IFormFile? ImageUpload)
         {
-            if (id != mochiPhoto.Id)
+            if (id != mochiPhoto.Id) return NotFound();
+
+            var existingPhoto = await _context.MochiPhoto.FindAsync(id);
+            if (existingPhoto == null) return NotFound();
+
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                return View(existingPhoto);
             }
 
-            if (ModelState.IsValid)
+            existingPhoto.Title = mochiPhoto.Title;
+            existingPhoto.PicTakenDate = mochiPhoto.PicTakenDate;
+            existingPhoto.UploadDate = mochiPhoto.UploadDate;
+
+            if (ImageUpload != null && ImageUpload.Length > 0)
             {
-                try
-                {
-                    _context.Update(mochiPhoto);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MochiPhotoExists(mochiPhoto.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                using var ms = new MemoryStream();
+                await ImageUpload.CopyToAsync(ms);
+                existingPhoto.ImageData = ms.ToArray();
             }
-            return View(mochiPhoto);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+
 
         // GET: MochiPhotos/Delete/5
         public async Task<IActionResult> Delete(int? id)
