5c18200378f3955b5bdc86969b3495e8d6864f58
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-25 10:54:45 -0400

    feat: setup gallery page and make video playback consistent

--- Models/MochiPhoto.cs
+++ Models/MochiPhoto.cs
@@ -5,6 +5,6 @@ public class MochiPhoto
     public int Id { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
-    public required string ImageFileName { get; set; }
+    public string? ImageFileName { get; set; }
     public DateTime UploadedAt { get; set; }
 }
